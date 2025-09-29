005a698e698a0fd1bce92183771487a9405e7111
emji\emji <michaeljaycatanguibagacina@gmail.com>
2025-09-29 11:32:27 +0800

    made violation user component

---
+++ app/Livewire/user/ViolationUserComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Component;
+
+class ViolationUserComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.violation-user-component');
+    }
+}
