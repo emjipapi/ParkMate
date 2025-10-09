469aa879ef784d802ae7dace4e0ecae723e9b23a
emji\emji <michaeljaycatanguibagacina@gmail.com>
2025-10-10 01:00:18 +0800

    removed wire navigate from admin create violation report

--- resources/views/admin/create-violation-report.blade.php
+++ resources/views/admin/create-violation-report.blade.php
@@ -552,7 +552,7 @@
         </div>
 
         <div class="position-absolute m-3 d-none d-md-block">
-            <a href="/violation-tracking" wire:navigate
+            <a href="/violation-tracking"
                 class="text-black d-inline-flex align-items-center justify-content-center border rounded-circle shadow"
                 style="width: 50px; height: 50px; font-size: 1.2rem; padding: 10px;">
                 <i class="bi bi-arrow-left"></i>
