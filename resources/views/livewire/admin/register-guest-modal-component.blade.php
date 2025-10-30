53c8ac2f1caef7306cd36d2f14af5867e216b2d1
emji\emji <michaeljaycatanguibagacina@gmail.com>
2025-10-30 17:54:50 +0800

    added clear info in register guest modal to clear inputs

--- resources/views/livewire/admin/register-guest-modal-component.blade.php
+++ resources/views/livewire/admin/register-guest-modal-component.blade.php
@@ -171,6 +171,9 @@
 
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-warning" wire:click="resetForm">
+                            <i class="bi bi-arrow-clockwise me-1"></i>Clear Info
+                        </button>
                         <button type="submit" class="btn btn-primary" wire:loading.attr="disabled" wire:target="registerGuest">
                             <span wire:loading.remove wire:target="registerGuest">Register Guest</span>
                             <span wire:loading wire:target="registerGuest">
